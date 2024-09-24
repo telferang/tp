7ed2a6e42ecbe6b020db5f701c473d558b3dc73f
telferang <telferang@gmail.com>
2024-09-24 18:10:45 +0800

    Add Parser class to Implement command parsing logic
    This structure allows the application to process user commands
    effectively and respond with appropriate actions or errors.
    
    By delegating command handling to specific command classes and
    validators, the design adheres to the Single Responsibility Principle,
    enhancing maintainability and scalability.

---
+++ src/main/java/seedu/budgetbuddy/Parser.java
@@ -0,0 +1,39 @@
+package seedu.budgetbuddy;
+
+import seedu.budgetbuddy.commands.Command;
+import seedu.budgetbuddy.commands.AddExpenseCommand;
+import seedu.budgetbuddy.commands.DeleteExpenseCommand;
+import seedu.budgetbuddy.commands.ListExpenseCommand;
+import seedu.budgetbuddy.commands.IncorrectCommand;
+import seedu.budgetbuddy.commands.AddIncomeCommand;
+import seedu.budgetbuddy.commands.DeleteIncomeCommand;
+import seedu.budgetbuddy.commands.ListIncomeCommand;
+import seedu.budgetbuddy.validators.AddExpenseValidator;
+import seedu.budgetbuddy.validators.AddIncomeValidator;
+import seedu.budgetbuddy.validators.DeleteExpenseValidator;
+import seedu.budgetbuddy.validators.DeleteIncomeValidator;
+
+public class Parser {
+    public Command parseCommand(String userCommandText) {
+        if (AddExpenseCommand.isCommand(userCommandText)) {
+            return AddExpenseValidator.processCommand(userCommandText);
+        }
+        if (DeleteExpenseCommand.isCommand(userCommandText)) {
+            return DeleteExpenseValidator.processCommand(userCommandText);
+        }
+        if (ListExpenseCommand.isCommand(userCommandText)) {
+            return new ListExpenseCommand();
+        }
+        if (AddIncomeCommand.isCommand(userCommandText)) {
+            return AddIncomeValidator.processCommand(userCommandText);
+        }
+        if (DeleteIncomeCommand.isCommand(userCommandText)) {
+            return DeleteIncomeValidator.processCommand(userCommandText);
+        }
+        if (ListIncomeCommand.isCommand(userCommandText)) {
+            return new ListIncomeCommand();
+        }
+        return new IncorrectCommand("Invalid input");
+    }
+
+}
