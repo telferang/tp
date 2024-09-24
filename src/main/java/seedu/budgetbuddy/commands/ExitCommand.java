569866656368eec06e9a3a251f4aecf0ac62878d
telferang <telferang@gmail.com>
2024-09-24 19:56:35 +0800

    Add exit command

--- src/main/java/seedu/budgetbuddy/commands/ExitCommand.java
+++ src/main/java/seedu/budgetbuddy/commands/ExitCommand.java
@@ -1,5 +1,19 @@
 package seedu.budgetbuddy.commands;
 
+import seedu.budgetbuddy.Ui;
+import seedu.budgetbuddy.expense.ExpenseManager;
+
 public class ExitCommand extends Command {
 
+    public ExitCommand() {
+
+    }
+
+    public static boolean isCommand(String command) {
+        return command.equals("bye");
+    }
+
+    public void execute() {
+        Ui.displayExitMessage();
+    }
 }
