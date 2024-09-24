eea17d4db926947b04d9163db9244c969f1c0024
telferang <telferang@gmail.com>
2024-09-24 18:23:00 +0800

    Add abstract Command class
    This Commands that will be inherited by other Command sub-classes
    for executing user commands
    
    This design allows for future command implementations to inherit from
    the Command class, promoting code reuse and adherence to object-oriented
     principles.

---
+++ src/main/java/seedu/budgetbuddy/commands/Command.java
@@ -0,0 +1,10 @@
+package seedu.budgetbuddy.commands;
+
+import seedu.budgetbuddy.Ui;
+
+public class Command {
+
+    public void execute() {
+        Ui.displayToUser("to be implemented by child classes");
+    }
+}
