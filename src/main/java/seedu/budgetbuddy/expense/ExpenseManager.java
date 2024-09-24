6cc0dbb3068f2488ee4678bca29b92b5390f498d
telferang <telferang@gmail.com>
2024-09-24 18:31:48 +0800

    Implement ExpenseManager and IncomeManager class for managing expenses and incomes transactions respectively
    The ExpenseManager / IncomeManager class help to facilitate adding,
    deleting, and listing expenses, along with maintaining the count of
    expenses
    
    This structure enhances organization and provides a straightforward
    interface for managing expenses or incomes transaction

---
+++ src/main/java/seedu/budgetbuddy/expense/ExpenseManager.java
@@ -0,0 +1,55 @@
+package seedu.budgetbuddy.expense;
+
+import seedu.budgetbuddy.Ui;
+
+import java.util.ArrayList;
+
+public class ExpenseManager {
+    private static int numberOfExpenses = 0;
+    private static ArrayList<Expense> expenses = new ArrayList<>();
+
+    /**
+     * Adds a new expense to the manager.
+     *
+     * @param expense The expense to be added.
+     */
+    public static void addExpense(Expense expense) {
+        expenses.add(expense);
+        numberOfExpenses++;
+        Ui.displayAcknowledgmentMessage(expense.toString(), "added", "expense", numberOfExpenses);
+    }
+
+    /**
+     * Deletes an expense from the manager at the specified index.
+     *
+     * @param index The index of the expense to be deleted.
+     */
+    public static void deleteExpense(int index) {
+        numberOfExpenses--;
+        Ui.displayAcknowledgmentMessage(expenses.get(index).toString(), "deleted", "expense", numberOfExpenses);
+        expenses.remove(index);
+    }
+
+    /**
+     * Returns the current number of expenses.
+     *
+     * @return The total number of expenses.
+     */
+    public static int getNumberOfExpenses() {
+        return numberOfExpenses;
+    }
+
+    /**
+     * Lists all the expenses managed by the manager.
+     * Displays each expense with its corresponding number.
+     */
+    public static void listExpenses() {
+        String result = "";
+        int counter = 1;
+        for (Expense expense : expenses) {
+            result += counter + ". " + expense.toString() + "\n";
+            counter++;
+        }
+        Ui.displayToUser(result);
+    }
+}
