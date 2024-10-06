00d74d5646a4dcc528d6793d69cf6fa4f8d74bc9
ChuaCleon <chuaweiqian.cleon@gmail.com>
2024-10-06 17:30:13 +0800

    Create new HelpCommand class to execute help command that displays message to guide new users. Integrate functionality to parse user input for help command in Parser class.

--- src/main/java/seedu/budgetbuddy/Parser.java
+++ src/main/java/seedu/budgetbuddy/Parser.java
@@ -1,14 +1,6 @@
 package seedu.budgetbuddy;
 
-import seedu.budgetbuddy.commands.Command;
-import seedu.budgetbuddy.commands.AddExpenseCommand;
-import seedu.budgetbuddy.commands.DeleteExpenseCommand;
-import seedu.budgetbuddy.commands.ListExpenseCommand;
-import seedu.budgetbuddy.commands.IncorrectCommand;
-import seedu.budgetbuddy.commands.AddIncomeCommand;
-import seedu.budgetbuddy.commands.DeleteIncomeCommand;
-import seedu.budgetbuddy.commands.ListIncomeCommand;
-import seedu.budgetbuddy.commands.ExitCommand;
+import seedu.budgetbuddy.commands.*;
 import seedu.budgetbuddy.validators.AddExpenseValidator;
 import seedu.budgetbuddy.validators.AddIncomeValidator;
 import seedu.budgetbuddy.validators.DeleteExpenseValidator;
@@ -51,6 +43,9 @@ public class Parser {
         }
         if (ExitCommand.isCommand(userCommandText)) {
             return new ExitCommand();
+        }
+        if (HelpCommand.isCommand(userCommandText)){
+            return new HelpCommand();
         }
         return new IncorrectCommand("Invalid input");
     }
