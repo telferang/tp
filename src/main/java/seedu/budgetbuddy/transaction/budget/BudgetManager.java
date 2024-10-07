3d6ab02fdd52fe13698331ad0bd5238ad4cbfc73
ZiliaAJY <141621081+ZiliaAJY@users.noreply.github.com>
2024-10-07 16:34:14 +0800

    Create method to list budget
    - Display up to 12 recent budgets or a specific budget for a given month.

--- src/main/java/seedu/budgetbuddy/transaction/budget/BudgetManager.java
+++ src/main/java/seedu/budgetbuddy/transaction/budget/BudgetManager.java
@@ -1,6 +1,7 @@
 package seedu.budgetbuddy.transaction.budget;
 
 import seedu.budgetbuddy.Ui;
+import seedu.budgetbuddy.transaction.expense.Expense;
 
 import java.time.YearMonth;
 import java.util.ArrayList;
@@ -60,5 +61,30 @@ public class BudgetManager {
             }
         }
         return null; // No budget found for the specified date
+    }
+
+    /**
+     * Lists all the budgets managed by the manager.
+     * Displays each budget with its corresponding number.
+     */
+    public static void listBudgets(YearMonth date) {
+        String result = "";
+        if (date == null) {
+            result += "Here are the budgets for the 12 most recent entries:\n";
+
+            int entriesToDisplay = Math.min(budgets.size(), 12);
+            for (int counter = 1; counter <= entriesToDisplay; counter++) {
+                Budget budget = budgets.get(budgets.size() - counter);
+                result += counter + ". " + budget.toString() + "\n";
+            }
+        } else {
+            // Assume validator guarantees date is valid
+            Budget budget = getBudget(date);
+            if (budget != null) {
+                result += "Here is the budget for the specified month:\n";
+                result += budget.toString();
+            }
+        }
+        Ui.displayToUser(result);
     }
 }
