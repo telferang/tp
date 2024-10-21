fa7cb6495a8effdb684c93a1a74a6d1f09d184d6
ZiliaAJY <141621081+ZiliaAJY@users.noreply.github.com>
2024-10-21 22:08:40 +0800

    Update JUnit tests to include categorising budgets

--- src/test/java/seedu/budgetbuddy/transaction/budget/BudgetTest.java
+++ src/test/java/seedu/budgetbuddy/transaction/budget/BudgetTest.java
@@ -11,56 +11,46 @@ public class BudgetTest {
 
     @Test
     public void testAddAmount_inputIsDouble_increasesBudgetTotalMonthlyBudget() {
-        Budget budget = new Budget(100.0, YearMonth.of(2024, 10));
+        Budget budget = new Budget(0, YearMonth.of(2024, 10));
         budget.addAmount(Category.FOOD,50.0);
-        assertEquals(150.0, budget.getTotalMonthlyBudget());
+        assertEquals(50.0, budget.getTotalMonthlyBudget());
     }
 
     @Test
     public void testDeductAmount_inputIsDouble_decreasesBudgetTotalMonthlyBudget() {
-        Budget budget = new Budget(100.0, YearMonth.of(2024, 10));
+        Budget budget = new Budget(0, YearMonth.of(2024, 10));
+        budget.addAmount(Category.OTHERS,100);
         budget.deductAmount(Category.OTHERS,30.0);
         assertEquals(70.0, budget.getTotalMonthlyBudget());
     }
 
     @Test
     public void testAddAmount_inputIsInt_increasesBudgetTotalMonthlyBudget() {
-        Budget budget = new Budget(100.0, YearMonth.of(2024, 2));
+        Budget budget = new Budget(0, YearMonth.of(2024, 2));
         budget.addAmount(Category.ENTERTAINMENT,150);
-        assertEquals(250.0, budget.getTotalMonthlyBudget());
+        assertEquals(150.0, budget.getTotalMonthlyBudget());
     }
 
     @Test
     public void testDeductAmount_inputIsInt_decreasesBudgetTotalMonthlyBudget() {
-        Budget budget = new Budget(100.0, YearMonth.of(2024, 2));
+        Budget budget = new Budget(0, YearMonth.of(2024, 2));
+        budget.addAmount(Category.TRANSPORT, 100);
         budget.deductAmount(Category.TRANSPORT,70);
         assertEquals(30.0, budget.getTotalMonthlyBudget());
     }
 
     @Test
-    public void testDeductAmount_inputIsDouble_budgetBecomesZeroWhenFullyDeducted() {
-        Budget budget = new Budget(100.0, YearMonth.of(2024, 10));
-        budget.deductAmount(Category.EDUCATION,100.0);
-        assertEquals(0.0, budget.getTotalMonthlyBudget());
-    }
-
-    @Test
-    public void testDeductAmount_inputIsInt_budgetBecomesZeroWhenFullyDeducted() {
-        Budget budget = new Budget(200.0, YearMonth.of(2024, 10));
-        budget.deductAmount(Category.FOOD,200);
-        assertEquals(0.0, budget.getTotalMonthlyBudget());
-    }
-
-    @Test
     public void testToString_returnsCorrectString() {
         Budget budget = new Budget(200.0, YearMonth.of(2024, 10));
+        budget.addAmount(Category.FOOD, 100);
         String result = budget.toString();
-        assertEquals("Amount: 200.0  Date: 2024-10", result);
+        assertEquals("Total Monthly Budget: 100.0  Date: 2024-10  Category: {FOOD=100.0}", result);
     }
 
     @Test
     public void testGetAmount_returnsCorrectTotalMonthlyBudget() {
-        Budget budget = new Budget(250.0, YearMonth.of(2024, 9));
+        Budget budget = new Budget(0, YearMonth.of(2024, 9));
+        budget.addAmount(Category.FOOD, 250);
         assertEquals(250.0, budget.getTotalMonthlyBudget());
     }
 
